8adcaf71a7e8e135ef5aaba16b533a969a84b6d1
Chris Kronberg <ckronber@gmail.com>
2021-06-21 13:38:42 -0400

    Delete Functionality Working

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -24,6 +25,22 @@ namespace RestaurantRater.Controllers
             return View();
         }
 
+        // GET: Restaurant/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            Restaurant restaurant = _db.Restaurants.Find(id);
+
+            if(restaurant == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(restaurant);
+        }
+
         // Post: Restaurant/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -36,6 +53,17 @@ namespace RestaurantRater.Controllers
                 return RedirectToAction("Index");
             }
             return View(restaurant);
+        }
+
+        // Post: Restaurant/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            _db.Restaurants.Remove(restaurant);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
